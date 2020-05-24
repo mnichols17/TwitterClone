6307c316be2db8f3e14d054dc49d4cc3f071808b
Mike <mike.nichols97@gmail.com>
2020-05-24 12:05:20 -0400

    MongoDB connected with mongoose

--- models/Account.js
+++ models/Account.js
@@ -1,1 +1,26 @@
-// Add account model
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const AccountSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = Account = mongoose.model('account', AccountSchema);
+
--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config()
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.log(err))
 
 app.use(express.json());
 
